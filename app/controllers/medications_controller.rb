ec7dbdb32ce7daa2ef9696f412f5060505cd0378
Kevin Kelley <kkelley622@gmail.com>
2023-04-19 14:38:20 -0500

    Added index, show, and create routes to medications

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,26 @@
 class MedicationsController < ApplicationController
+    before_action :find_medication, only: [:show]
+    def index 
+        medications = Medication.all 
+        render json: medications, status: :ok 
+    end
+
+    def show 
+        render json: @medication, status: :ok 
+    end
+
+    def create 
+        medication = Medication.create!(medication_params)
+        render json: medication, status: :created
+    end
+
+    private 
+
+    def find_medication 
+        @medication = Medication.find(params[:id])
+    end
+
+    def medication_params 
+        params.permit(:brand_name, :generic_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show]
   resources :procedures
   resources :appointments
+  resources :medications
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
