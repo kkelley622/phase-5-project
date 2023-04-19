22f836e5b388ed8d61d0fcacf8f988ba09cf6348
Kevin Kelley <kkelley622@gmail.com>
2023-04-19 17:39:40 -0500

    Added index, show and create methods to problems controller

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,3 +1,33 @@
 class ProblemsController < ApplicationController
 
+    before_action :find_problem, only: [:show]
+
+    def index 
+        if params[:user_id]
+            user = User.find_by_id(params[:user_id])
+            @problems = user.problems 
+        else
+            @problems = Problem.all 
+        end
+        render json: @problems, status: :ok
+    end
+
+    def show
+        render json: @problem, status: :ok
+    end
+
+    def create 
+        problem = Problem.create!(problem_params)
+        render json: problem, status: :created
+    end
+
+    private 
+
+    def find_problem
+        @problem = Problem.find(params[:id])
+    end
+
+    def problem_params
+        params.permit(:user_id, :description)
+    end
 end
