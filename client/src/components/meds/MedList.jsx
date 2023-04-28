4e4b77afd02ab2641b954ccc1f600662feda2835
Kevin Kelley <kkelley622@gmail.com>
2023-04-28 09:39:23 -0600

    Successfully displaying list of medications.

--- client/src/components/meds/MedList.jsx
+++ client/src/components/meds/MedList.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const MedList = () => {
+  const meds = useSelector(store => store.medsReducer)
+  console.log("meds list", meds)
+
+  const medsList = meds.map(med => <li key={med.id}>{med.generic_name} ({med.brand_name})</li>)
+
   return (
-    <div>MedList</div>
+    <ul>{ medsList }</ul>
   )
 }
 
