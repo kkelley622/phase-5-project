39165fba6d8356cd7d5da550befc74aeec939488
Kevin Kelley <kkelley622@gmail.com>
2023-05-17 22:33:56 -0500

    Added use effect to procedures list to reroute user if attempting to access without being logged in.

--- client/src/components/procedures/ProceduresList.jsx
+++ client/src/components/procedures/ProceduresList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import ProcedureCard from './ProcedureCard'
+import { useNavigate } from 'react-router-dom';
 
 const ProceduresList = () => {
+  const { loggedIn} = useSelector(store => store.usersReducer);
   const procedures = useSelector(store => store.proceduresReducer);
+  const navigate= useNavigate();
+
+  useEffect(() => {
+    if(!loggedIn) {
+      navigate('/login')
+    }
+  }, [loggedIn, navigate])
   
   const proceduresList = procedures.map(procedure => <ProcedureCard key={procedure.id} procedure={procedure}/>)
 
