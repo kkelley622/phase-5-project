a747a0e81d36e64990237578639c73f526ea51eb
Kevin Kelley <kkelley622@gmail.com>
2023-05-17 22:24:02 -0500

    Added use effect to reroute user if attempting to acces appointments without being logged in.

--- client/src/components/appointments/AppointmentsList.jsx
+++ client/src/components/appointments/AppointmentsList.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import AppointmentCard from './AppointmentCard';
+import { useNavigate } from 'react-router-dom';
 
 const AppointmentsList = () => {
   const appointments = useSelector(store => store.appointmentsReducer);
-
+  const { loggedIn } = useSelector(store => store.usersReducer);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if(!loggedIn) {
+      navigate('/login')
+    }
+  }, [loggedIn, navigate]);
+  
   const appointmentsList = appointments.map(app => <AppointmentCard key={app.id} app={app}/>)
+
 
   return (
     <div>{appointmentsList}</div>
