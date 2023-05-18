27379a15cc2ce70efc6d2980771a27550ac89961
Kevin Kelley <kkelley622@gmail.com>
2023-05-17 22:27:06 -0500

    Added use effect to prescriptions list to reroute user if attempting to access without being logged in.

--- client/src/components/prescriptions/PrescriptionsList.jsx
+++ client/src/components/prescriptions/PrescriptionsList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import PrescriptionCard from './PrescriptionCard'
+import { useNavigate } from 'react-router-dom';
 
 const PrescriptionsList = () => {
-    const prescriptions = useSelector(store => store.prescriptionsReducer)
+    const { loggedIn } = useSelector(store => store.usersReducer);
+    const navigate = useNavigate();
+    const prescriptions = useSelector(store => store.prescriptionsReducer);
+
+    useEffect(() => {
+      if(!loggedIn) {
+        navigate('/login')
+      }
+    }, [loggedIn, navigate])
 
     const prescriptionsList = prescriptions?.map(prescription => <PrescriptionCard key={prescription.id} prescription={prescription}/>)
 
