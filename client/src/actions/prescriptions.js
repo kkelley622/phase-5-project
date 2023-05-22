23d67e7b3130511079f0da108284c511cd65c0a6
Kevin Kelley <kkelley622@gmail.com>
2023-05-22 17:32:01 -0500

    Added addPrescription function to prescription.js.

--- client/src/actions/prescriptions.js
+++ client/src/actions/prescriptions.js
@@ -9,6 +9,28 @@ export const loadPrescriptions = () => {
     }
 };
 
+export const addPrescription = (prescription) => {
+    return dispatch => {
+        fetch('/prescriptions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(prescription)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(!data.errors) {
+                const action = { type: 'ADD_PRESCRIPTION', payload: data}
+                dispatch(action)
+                dispatch(clearErrors())
+            } else {
+                dispatch(setErrors(data.errors))
+            }
+        })
+    }
+}
+
 export const deletePrescription = (id) => {
     return dispatch => {
         fetch(`/prescriptions/${id}`, {
