f5a246c150144a451aca8507c5f4ee00ec7bdc42
Kevin Kelley <kkelley622@gmail.com>
2023-05-22 15:24:37 -0500

    Added add problem case to problems reducer.

--- client/src/components/reducers/problemsReducer.js
+++ client/src/components/reducers/problemsReducer.js
@@ -1,9 +1,13 @@
+import { addResource } from "../../Globals";
+
 const initialState = [];
 
 const problemsReducer = (state=initialState, action) => {
     switch(action.type){
         case "LOAD_PROBLEMS":
             return action.payload
+        case "ADD_PROBLEM":
+            return addResource(state, action.payload)
         case 'DELETE_PROBLEM':
             return state.filter(problem => problem.id !== action.payload)
         default:
