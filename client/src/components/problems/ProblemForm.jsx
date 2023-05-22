4ea3966027e570df25dc580bacd578d7d7540cad
Kevin Kelley <kkelley622@gmail.com>
2023-05-22 15:36:24 -0500

    Passing addProblem to handleSubmit function in ProblemForm. Successully adds problem to state and back end db.

--- client/src/components/problems/ProblemForm.jsx
+++ client/src/components/problems/ProblemForm.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { addProblem } from '../../actions/problems'
+import { useDispatch } from 'react-redux';
 
 const ProblemForm = () => {
-    const [description, setDescription] = useState();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    description: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addProblem(formData))
+  }
 
   return (
-    <form>Add a New Problem or Diagnosis:
+    <form onSubmit={handleSubmit}>Add a New Problem or Diagnosis:
         <br/>
         <label>Problem/Diagnosis</label>
         <input 
             type="text"
             name="description"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
+            value={formData.description}
+            onChange={handleChange}
         />
         <button>Add Problem</button>
     </form>
