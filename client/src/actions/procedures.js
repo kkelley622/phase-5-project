6ffffee9c7640510e2ef954b6a1da5c6a8a8bede
Kevin Kelley <kkelley622@gmail.com>
2023-05-22 16:23:30 -0500

    Added addProcedure function to procedures.js.

--- client/src/actions/procedures.js
+++ client/src/actions/procedures.js
@@ -1,3 +1,5 @@
+import { clearErrors, setErrors } from "./errors";
+
 export const loadProcedures = () => {
     return dispatch => {
         fetch('/procedures')
@@ -5,6 +7,28 @@ export const loadProcedures = () => {
         .then(data => {
             const action = { type: "LOAD_PROCEDURES", payload: data}
             dispatch(action)
+        })
+    }
+};
+
+const addProcedure = (procedure) => {
+    return dispatch => {
+        fetch('/procedures', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(procedure)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(!data.errors) {
+                const action = { type: 'ADD_PROBLEM', payload: data}
+                dispatch(action)
+                dispatch(clearErrors())
+            } else {
+                dispatch(setErrors(data.errors))
+            }
         })
     }
 };
