1dccac44a695f84f4f2135439527b8dc0dd6b216
Kevin Kelley <kkelley622@gmail.com>
2023-05-24 16:54:01 -0500

    Added dropdown select options to choose from providers list when adding an appointment.

--- client/src/components/appointments/AppointmentForm.jsx
+++ client/src/components/appointments/AppointmentForm.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { addAppointment } from '../../actions/appointments';
-import {  useDispatch } from 'react-redux';
+import {  useDispatch, useSelector } from 'react-redux';
 
 
 const AppointmentForm = () => {
+    const providers = useSelector(store => store.providersReducer);
     const dispatch = useDispatch();
     const [formData, setFormData] = useState({
         date: "",
@@ -23,7 +24,9 @@ const AppointmentForm = () => {
             location: "",
             provider_id: ""
         })
-    }
+    };
+
+    const providersList = providers.map(provider => <option key={provider.id} value={provider.id}>{provider.last_name}, {provider.first_name}</option>)
 
   return (
     <form onSubmit={handleSubmit}>
@@ -42,13 +45,20 @@ const AppointmentForm = () => {
             value={formData.location}
             onChange={handleChange}
         />
-        <label>Provider</label>
-        <input
+        <label htmlFor='provider_id'>Provider</label>
+        <select 
+            id="provider_id"
+            name="provider_id"
+            value={formData.provider_id}
+            onChange={handleChange}>
+            {providersList}
+        </select>
+        {/* <input
             type="text"
             name="provider_id"
             value={formData.provider_id}
             onChange={handleChange}
-        />
+        /> */}
         <button>Submit</button>
     </form>
   )
